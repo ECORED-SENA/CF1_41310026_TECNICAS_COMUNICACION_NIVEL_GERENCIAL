dcd8a5e84073d4c52b44d8c59ee2863d6dfe431b
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-09-16 22:32:41 -0500

    avances

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,97 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.pb-0.pb-md-0
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Tipos de comunicación
+    
+    .row.align-items-center.mb-4
+      .col-md-auto(data-aos="fade-right")
+        figure.mb-4.mb-md-0
+          img.mx-auto(src="@/assets/curso/temas/21.svg",style="max-width: 88px;")
+      .col(data-aos="fade-left")
+        p.mb-0 Comprender los tipos de comunicación y su aplicación en el entorno laboral es crucial para que todos los actores dentro de una organización operen de manera sincronizada, tanto interna como externamente. Esta claridad facilita que los planes de comunicación interna y las herramientas de gestión generen los resultados esperados, como se presenta en la siguiente gráfica.
+    
+    .row.justify-content-center(data-aos="flip-up")
+      .col-lg-10
+        .fondo-color-sistema-n.p-4
+          .row.align-items-center
+            .col-lg-3.d-none.d-lg-block
+              figure.mb-4.mb-lg-0
+                img.mx-auto(src="@/assets/curso/temas/22.svg",style="max-width: 224px;")
+            .col-lg-9
+              .titulo-sexto.color-acento-contenido
+                p.mb-0 #[b Figura 1.] Tipos de comunicación
+              
+              figure
+                img.mx-auto.d-none.d-lg-block(src="@/assets/curso/temas/23.svg",style="max-width: 715px;",alt="El diagrama presenta la interrelación entre la organización empresarial y la comunicación, destacando la comunicación interna y externa como elementos clave que se conectan directamente con el núcleo de la comunicación dentro de la empresa.")
+                img.mx-auto.d-lg-none(src="@/assets/curso/temas/24.svg",style="max-width: 350px;",alt="El diagrama presenta la interrelación entre la organización empresarial y la comunicación, destacando la comunicación interna y externa como elementos clave que se conectan directamente con el núcleo de la comunicación dentro de la empresa.")
+    
+    .titulo-decorativo.my-5(data-aos="fade-up")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Comunicación interna y externa
+    
+    .row.align-items-center.mb-4
+      .col-lg-4(data-aos="fade-up")
+        figure.mb-4.mb-lg-0
+          img.mx-auto(src="@/assets/curso/temas/25.svg",style="max-width: 398px;")
+      .col-lg-8(data-aos="fade-down")
+        .row.align-items-center.mb-4
+          .col-lg-7.mb-4.mb-lg-0
+            .fondo-color-sistema-m.p-4.borde-17
+              figure.mb-4
+                img.me-auto(src="@/assets/curso/temas/26.svg",style="max-width: 69px;")
+              p.mb-0 Uno de los aspectos fundamentales en una organización es la comunicación. En este contexto, el juego y la diversión pueden convertirse en herramientas de comunicación interna que fomentan el compañerismo, la creatividad y la motivación. Este tipo de dinámica comunicativa, conocida como comunicación interna, es esencial para fortalecer los lazos entre los diversos miembros de una empresa.
+          .col-lg-5
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/27.png",style="max-width: 294px;")
+        
+        .row
+          .col-md-auto.d-none.d-lg-block
+            figure
+              img.mx-auto(src="@/assets/curso/temas/1.svg",style="max-width: 85px;")
+          .col
+            p.mb-0 Los empleados de la organización se convierten, así, en un público interno tan importante como los destinatarios de los mensajes publicitarios externos. Formar parte de la realidad corporativa permite identificar tanto las virtudes como los defectos de la empresa. Mediante estrategias de comunicación adecuadas, se refuerza la imagen corporativa para compartirla con el exterior.
+    
+    p.mb-4(data-aos="fade") Por este motivo, todo plan de comunicación externa debe estar alineado con un plan de comunicación interna. La relevancia de esta alineación radica en que permite:
+
+    .fondo-personalizado-1.f2
+      .fondo-personalizado-1__contenido.py-5
+        .bg-white.borde-20.p-4(data-aos="zoom-in-out")
+          SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+            .row.justify-content-center.align-items-center
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4.pt-lg-4 Involucrar al personal
+                p.mb-0 Una empresa necesita personal que la conforme, y estos empleados son quienes transmiten los valores y objetivos empresariales. Cada empleado tiene un rol vital y, al reconocer su importancia, se les hace sentir parte integral de la organización.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/29.png",style="max-width: 493px;")
+            .row.justify-content-center.align-items-center
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4.pt-lg-4 Ser coherente y coordinar
+                p.mb-0 Un plan de acción bien coordinado requiere estrategias de comunicación coherentes, manteniendo un flujo constante de información entre los diferentes departamentos de la organización.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/31.png",style="max-width: 493px;")
+            .row.justify-content-center.align-items-center
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4.pt-lg-4 Participar y adaptarse al cambio
+                p.mb-0 Las transformaciones que se generan a medida que la empresa se expande a nivel local, nacional o internacional exigen una gran adaptabilidad y un esfuerzo coordinado de comunicación y capacitación para mantenerse en el camino del cambio.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/32.png",style="max-width: 493px;")
+            .row.justify-content-center.align-items-center
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4.pt-lg-4 Activar la productividad
+                p.mb-0 Para optimizar los procesos productivos, no solo es necesaria una comunicación adecuada, sino también la participación activa del personal en los resultados de la organización. Es fundamental que cada empleado, colaborador o usuario de la empresa entienda no solo qué tiene que hacer y cómo hacerlo, sino también por qué y para qué lo hace.
+              .col-lg-5
+                figure
+                  img.mx-auto(src="@/assets/curso/temas/33.png",style="max-width: 493px;")
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,11 +1,65 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.pb-0.pb-md-0
+    .titulo-principal.color-acento-contenido.mb-4(data-aos="flip-up")
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Plan de comunicación interna
+    
+    .fondo-personalizado-1.pos-bottom.size-full.f3
+      .fondo-personalizado-1__contenido.pb-5
+        .row.mb-4
+          .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
+            .fondo-color-sistema-k.p-4.borde-15.h-100.align-content-center
+              p.mb-0 Toda organización es el resultado de la participación activa de diversas partes, incluidas sus objetivos, mecanismos organizativos y estructura económica. La comunicación es un mecanismo que atraviesa la institución en diversas dimensiones. A partir de ello, se pueden identificar diferentes ámbitos de la comunicación en las organizaciones, como se presenta a continuación:
+          .col-lg-7(data-aos="fade-left")
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/34.png",style="max-width: 715px;")
+        
+        TabsC.color-acento-contenido.tabs-c-p-1.mb-4(data-aos="zoom-in-out")
+          .bg-white.py-5.px-4(titulo="Ámbito de la información")
+            .row.align-items-center
+              .col-lg-5
+                figure.mb-4.mb-lg-0
+                  img.mx-auto(src="@/assets/curso/temas/35.png",style="max-width: 424px;") 
+              .col-lg-7
+                h4.mb-4 Ámbito de la información
+                p.mb-0 Este ámbito incluye las normas y datos que la empresa emplea según sus funciones y objetivos definidos. 
+          .bg-white.py-5.px-4(titulo="Ámbito de divulgación")
+            .row.align-items-center
+              .col-lg-5
+                figure.mb-4.mb-lg-0
+                  img.mx-auto(src="@/assets/curso/temas/37.png",style="max-width: 424px;") 
+              .col-lg-7
+                h4.mb-4 Ámbito de divulgación
+                p.mb-0 Comprende diversos elementos destinados a captar, convencer, persuadir y sumar la voluntad de las personas a los fines de la organización. Para este objetivo, se utilizan boletines, publicaciones y comunicados.
+          .bg-white.py-5.px-4(titulo="Ámbito de formación y socialización")
+            .row.align-items-center
+              .col-lg-5
+                figure.mb-4.mb-lg-0
+                  img.mx-auto(src="@/assets/curso/temas/38.png",style="max-width: 424px;") 
+              .col-lg-7
+                h4.mb-4 Ámbito de formación y socialización
+                p.mb-0 Refuerza de manera explícita la cultura institucional. En este ámbito se insertan actividades de capacitación, recreación y eventos institucionales.
+          .bg-white.py-5.px-4(titulo="Ámbito de participación")
+            .row.align-items-center
+              .col-lg-5
+                figure.mb-4.mb-lg-0
+                  img.mx-auto(src="@/assets/curso/temas/39.png",style="max-width: 424px;") 
+              .col-lg-7
+                h4.mb-4 Ámbito de participación
+                p.mb-0 Permite que los miembros de la empresa se expresen a través de diferentes modos de comunicación. Este tipo de dinámica abre espacio para el reconocimiento y la colaboración de los diversos actores que conforman la empresa. Involucra el trabajo en equipo, el reconocimiento de iniciativas, sugerencias y propuestas.
+    
+        .row.justify-content-center(data-aos="fade")
+          .col-lg-10
+            .fondo-color-acento-contenido.borde-16.p-4
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/40.svg",style="max-width: 85px;")
+                .col
+                  p.mb-0 Para la ejecución de un plan de comunicación interna, es esencial una planificación previa que permita su implementación y posterior evaluación. Todo plan de comunicación interna debe contar con el respaldo de la máxima autoridad. De lo contrario, todo el esfuerzo y planificación serán en vano. El plan debe incluir objetivos claros como comunicar la visión, misión, política de calidad y valores de la organización.
 
 </template>
 
